d6991f576418a5db0639217e9da3bf33eb8e7a42
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-03-21 02:59:33 +0300

    Create 40.lisp

---
+++ 40.lisp
@@ -0,0 +1,30 @@
+(defun del (1st 2nd)
+    (
+        (lambda (first rest)
+            (cond
+                ((null 1st) NIL)
+                ((equalp first 2nd) rest)
+                (t (cons first (del rest 2nd)))
+            )   
+        )
+        (car 1st) (cdr 1st)
+    )
+)
+ 
+(defun dif (source e)
+    (
+        (lambda (first_e rest_e)
+            (cond
+                ((null e) source)
+                (t (dif (del source first_e) rest_e))
+            )
+        )
+        (car e) (cdr e)
+    )
+)
+
+(print (dif `(1 2 3 4) `(2 3 4 5)))
+(1)
+
+(print (dif `(Hello My World) `(My)))
+(HELLO WORLD)
