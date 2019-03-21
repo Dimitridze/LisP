e1f0f290576427e48169891e342fa53d45d289b6
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-03-21 10:18:26 +0300

    Update 3.lisp

--- 3.lisp
+++ 3.lisp
@@ -1,20 +1,20 @@
-Задача 3
+;Задача 3
 
-Определите функцию, заменяющую в исходном списке все вхождения заданного значения другими
+;Определите функцию, заменяющую в исходном списке все вхождения заданного значения другими
 
  (defun rep (lst w &key (r 0))
     ((lambda (first rest)
         (cond ((null lst) nil)
-        (( eq first w) (cons r (rep rest w r)))
-        ((listp first) (cons (rep first w r) (rep rest w r)))
-        (t (cons first (rep rest w r))))
+           (( eq first w) (cons r (rep rest w r)))
+           ((listp first) (cons (rep first w r) (rep rest w r)))
+           (t (cons first (rep rest w r))))
      )
      
      (car lst)(cdr lst)
    )
 )
         
- ; ( print (rep '(1 2 3 1 1) 1 'a) )
-  ;  (A 2 3 A A) 
-;  ( print (rep '((1 2 3) 4 5 6 1) 1 44) )
-;    ((44 2 3) 4 5 6 44)
+;(print (rep '(1 2 3 1 1) 1 'a))
+;(A 2 3 A A) 
+;(print (rep '((1 2 3) 4 5 6 1) 1 44))
+;((44 2 3) 4 5 6 44)
