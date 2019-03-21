90587b81855d3b7fe78b97c4097ea5d8dd5b516d
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-03-21 03:40:10 +0300

    Create 17.lisp

---
+++ 17.lisp
@@ -0,0 +1,11 @@
+(defun transposition (w) 
+   (cond ((null w) nil) 
+     ((null (cdr w)) (list w)) 
+       ((loop for a in w 
+
+        nconc (mapcar #'(lambda (e) (cons a e)) (transposition (remove a w))))) 
+   ) 
+) 
+
+(print (transposition '(1 2 3)))
+((1 2 3) (1 3 2) (2 1 3) (2 3 1) (3 1 2) (3 2 1)) 
