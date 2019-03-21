f6d3e672fb5217cddad195720ee48a1572bfbf8d
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-03-21 10:19:38 +0300

    Update 28.lisp

--- 28.lisp
+++ 28.lisp
@@ -1,28 +1,22 @@
-
 Задача 28
 
 Определите функцию, вычисляющую, сколько всего атомов в списке.
 
 (defun hm(lst &optional (kolv 0)) 
    ((lambda(tail) 
-     (cond 
-       ((null lst) kolv) 
-
-       ((atom (car lst)) (hm tail (+ kolv 1))) 
-
-       (t (hm tail kolv)) 
-
-     )
+       (cond 
+           ((null lst) kolv) 
+           ((atom (car lst)) (hm tail (+ kolv 1))) 
+           (t (hm tail kolv)) 
+       )
     )
-         (cdr lst)
-   )   
+    (cdr lst)
+    )   
 ) 
 
-    ;(print(hm `(1 a 3 `(e2 e4)) 0)) 
+;(print(hm `(1 a 3 `(e2 e4)) 0)) 
+;3
 
-    ;3
-
-    ;(print(hm () 0)) 
-
-   ; 0
+;(print(hm () 0)) 
+;0
 
