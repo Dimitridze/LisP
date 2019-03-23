497332b3394001579e8677c314a9f10dafddfd2e
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-03-23 22:55:15 +0300

    Update 45.lisp

--- 45.lisp
+++ 45.lisp
@@ -1,20 +1,24 @@
 ;45. Напишите функцию (РАССТОЯНИЕ c1 c2), вычисляющую расстояние между городами.
 
-(defun Road (c1 &optional(c2 `Dzhankoi)) 
+(defun Road (c1 &optional(c2 `Bahchisaray)) 
   (sqrt (+ (expt (- (get c1 'x) (get c2 'x)) 2)
            (expt (- (get c1 'y) (get c2 'y)) 2))
   )
 )
  
-(setf (get 'New_York 'x) 1770)
-(setf (get 'New_York 'y) -445)
- 
-(setf (get 'Dzhankoi 'x) -300)
-(setf (get 'Dzhankoi 'y) 50)
+(defun City(Sym &key x y ) 
+    (setf (get Sym 'x) x)
+    (setf (get Sym 'y) y)
+  
+)
 
-;(print(Road 'New_York 'Dzhankoi))
+(City  'New_York :x 1770 :y -445)
+(City  'Bahchisaray :x -300 :y 50)
+
+
+;(print(Road 'New_York 'Bahchisaray))
 ;2128.362 
-;(print(Road 'Dzhankoi ))
+;(print(Road 'Bahchisaray ))
 ;0
-;(print(Road 'Dzhankoi 'New_York))
+;(print(Road 'Bahchisaray 'New_York))
 ;2128.362 
