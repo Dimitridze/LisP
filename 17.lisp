d7c7131693cfba145b56ab168d90dec71a4ecbc6
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-03-27 23:01:39 +0300

    Update 17.lisp

--- 17.lisp
+++ 17.lisp
@@ -2,15 +2,25 @@
 
 ;Создайте предикат, порождающий всевозможные перестановки исходного множества
 
-(defun transposition (w) 
-  (cond ((null w) nil) 
-     ((null (cdr w)) (list w)) 
-     
-         ((loop for a in w 
-
-          nconc (mapcar #'(lambda (e) (cons a e)) (transposition (remove a w))))) 
-  ) 
-) 
+(defun ins-in-all-pos (a lst r)
+  (cond ((null r) (list (append lst (list a))
+                  )
+        )
+              (t (cons (append lst (list a) r) (ins-in-all-pos a (append lst (list (car r))) (cdr r))
+                       
+        )
+     )
+  )
+)
+ 
+ 
+(defun all-perms (lst)
+  (cond ((null (cdr lst)) (list lst))
+     (t (apply 'append (mapcar (lambda (x) (ins-in-all-pos (car lst) nil x)) (all-perms (cdr lst)))
+        )
+     )
+  )
+)
 
 ;(print (transposition '(1 2 3)))
 ;((1 2 3) (1 3 2) (2 1 3) (2 3 1) (3 1 2) (3 2 1)) 
