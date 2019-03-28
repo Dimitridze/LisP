56dd978187c2949a6121cb61930ece45bdf7400d
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-03-28 08:57:27 +0300

    Update 46.lisp

--- 46.lisp
+++ 46.lisp
@@ -4,36 +4,41 @@
 ;Напишите функцию (родители x), которая возвращает в качестве значения родителей, и предикат (сестры-братья x1 x2), 
 ;который истинен в случае, если x1 и x2 — сестры или братья, родные или с одним общим родителем.
 
- (defun setParents(human mom dad)
-	  (setf (get human `mom) mom)
-	  (setf (get human `dad) dad)
- )
+(defun mother(person)
+	(get person `mother)
+)
 
+(defun father(person)
+	(get person `father)
+)
 
-(defun mother(human)
-	(get human `mom)
-  )
-(defun father(human)
-	(get human `dad)
-  )
+(defun set-parents(person mother father)
+	(setf (get person `mother) mother)
+	(setf (get person `father) father)
+)
 
-(defun fam(1sthuman 2ndhuman)
+(defun get-parents(person)
+	(list (mother person) (father person))
+)
+
+(defun is-family(person1 person2)
 	(cond
-		((eq (mother 1sthuman) (mother 2ndhuman) t)
-		((eq (father 1sthuman) (father 2ndhuman) t)
+		((eq (mother person1) (mother person2)) t)
+		((eq (father person1) (father person2)) t)
 		(t nil)
-)
-  
+	)
 )
 
-  
- (defun prog(1sthuman 2ndhuman firstP secondP)
-	 (setParents 1sthuman (car firstP) (cadr firstP))
-	 (setParents 2ndhuman (car secondP) (cadr secondP))
-	 (print(par 1sthuman))
-	 (print(par 2ndhuman))
-	 (print(fam 1sthuman 2ndhuman))
- )
+(defun prog(person1 person2 parents1 parents2)
+	(set-parents person1 (car parents1) (cadr parents1))
+	(set-parents person2 (car parents2) (cadr parents2))
+	(print(get-parents person1))
+	(print(get-parents person2))
+	(print(is-family person1 person2))
+)
+ 
+
+
 
  
 
