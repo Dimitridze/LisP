6d45ffad71e4f4e3e864e8c37fb0a2732525ace7
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-04-04 00:27:03 +0300

    Update 17.lisp

--- 17.lisp
+++ 17.lisp
@@ -2,26 +2,59 @@
 
 ;Создайте предикат, порождающий всевозможные перестановки исходного множества
 
-(defun ins-in-all-pos (a lst r)
-  (cond ((null r) (list (append lst (list a))
-                  )
-        )
-              (t (cons (append lst (list a) r) (ins-in-all-pos a (append lst (list (car r))) (cdr r))
-                       
-        )
-     )
-  )
-)
- 
- 
-(defun all-perms (lst)
-  (cond ((null (cdr lst)) (list lst))
-     (t (apply 'append (mapcar (lambda (x) (ins-in-all-pos (car lst) nil x)) (all-perms (cdr lst)))
-        )
-     )
-  )
+(defun permutation (lst)
+    
+    (cond
+        ( (null lst) nil)
+        ( (null (cdr lst) ) (list lst))
+        ( t (add-elem-to-each-permutation (car lst) (permutation (cdr lst) ) ) )
+    )
 )
 
+(defun add-elem-to-each-permutation (elem perm-lst)
+    
+    (cond
+        ( (null perm-lst) nil)
+        (t (append
+                (insert-elem-to-each-pos elem (car perm-lst))
+                (add-elem-to-each-permutation elem (cdr perm-lst) ) 
+) 
+        )
+     )
+)   
+        
+(defun insert-elem-to-each-pos (elem lst)
+    
+    (cond
+        ( (null lst) (list elem) )
+        ( (atom lst) (insert-elem-to-each-pos elem (list lst) ) )
+        ( t (cons 
+             (cons elem lst) (insert-elem-to-each-pos-aux elem nil lst) ) )
+    )
+)
+
+(defun insert-elem-to-each-pos-aux (elem lst1 lst2)
+    
+    (cond
+        ( ( null lst2) nil)
+        ( t ( (lambda (a)
+                (cons (append (car a) (list elem) (cadr a))
+                    
+                      ( (lambda (x)
+                        (insert-elem-to-each-pos-aux elem (car x) (cadr x)))
+                    
+                       a))
+            )
+                      
+             ( (lambda (lst1 lst2)
+                (list (append lst1 (list (car lst2))) (cdr lst2))
+              
+              ) lst1 lst2)))
+    )
+)
+
+
+(print (permutation '(1 2 3 4)))
 ;(print (transposition '(1 2 3)))
 ;((1 2 3) (1 3 2) (2 1 3) (2 3 1) (3 1 2) (3 2 1)) 
 
