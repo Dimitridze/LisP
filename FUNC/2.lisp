ea3317a40cc6f10c659cc6ac7c1ea6954bd3bb7c
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-04-11 09:10:04 +0300

    Update 2.lisp

--- FUNC/2.lisp
+++ FUNC/2.lisp
@@ -3,12 +3,14 @@
 (defun maplist (fn lst)
 
           (cond 
-             ((null lst) nil)
              ((null fn) nil)
-             (t(list (funcall fn lst) (maplist fn (cdr lst))))
+             ((null lst) nil)
+            
+             (t(cons (funcall fn lst)   (maplist fn (cdr lst))))    ;->((3 2 1) (3 2) (3)) 
+            ;(t(append (funcall fn lst) (maplist fn (cdr lst))))    ;->(3 2 1 3 2 3)
           )
 )
 
 
 (print (maplist 'REVERSE '(1 2 3)))
-;((3 2 1) ((3 2) ((3) NIL))) 
+;((3 2 1) (3 2) (3))  
