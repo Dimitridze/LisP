b87916e96a46904018c0594e677db89cd680c470
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-04-12 15:46:57 +0300

    Update test2.lisp

--- FUNC/test2.lisp
+++ FUNC/test2.lisp
@@ -1,6 +1,11 @@
-( defun del-if(pred lst)
+;6.Определите фильтр (удал пред список), 
+;удаляющий из списка все элементы, которые обладают свойством наличие которого определяет предикат пред
+(defun del-if (pred lst)
     (mapcan #'(lambda(x)
-                   (if (funcall pred x) (list x) nil)) lst)
+                   (if (funcall pred x) nil (list x))) lst)
 )
 
 (print(del-if 'evenp '( 1 3 4 5 )))
+;(1 3 5)
+(print(del-if 'numberp '( a 1 b 2 c 3)))
+;(A B C)
