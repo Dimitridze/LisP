e4451d19ccd6a2e5d06e50e4eacbe0c0e2d17a1c
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-04-18 09:28:02 +0300

    Update 4.lisp

--- FUNC/4.lisp
+++ FUNC/4.lisp
@@ -1,14 +1,28 @@
 ;4. Определите функциональный предикат(КАЖДЫй пред список), который истинен в том и только в том случае, 
 ;когда, являющейся функциональным аргументом предикат пред истинен для всех элементов списка.
 
-(defun every1 (pred ls)
-  (not (member nil (mapcar pred ls)))
+;(defun every1 (pred ls)
+;  (not (member nil (mapcar pred ls)))
+;)
+
+(defun every1 (pred lst)
+    (if
+        (null
+            (mapcan (lambda (x)  
+                            (if (funcall pred x) NIL (list T)                             
+                            )
+                    ) lst
+            ) 
+        ) 
+     T NIL
+    )
 )
-   
-(print (every1 'evenp  '(8 10 12)))
+
+(print (every1 'evenp  '(8 10  12)))
 ;T
-(print (every1 'evenp  '(7 8 9 10)))
+(print (every1 'evenp  '(8 10 11 12)))
 ;NIL
+   
 (print (every1 'numberp  '(8 10 12)))
 ;T
 (print (every1 'numberp  '(8 a 10 12)))
