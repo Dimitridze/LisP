8ad191c2020434c184f57362f159aec49f89580c
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-04-18 11:03:21 +0300

    Update 43.lisp

--- 43.lisp
+++ 43.lisp
@@ -1,21 +1,29 @@
 ;43.Определите функцию, подсчитывающую количество всех вершин данного дерева заданной высоты
 
-(defun tree-count (tree lv)
+(defun tree-count (tree lvl)
   (cond ((null tree) 0)
-        ((zerop lv) 1)
-        (t (+ (tree-count (car tree) (- lv 1))
-              (tree-count (caddr tree) (- lv 1))))))
- 
+        ((= lvl 0) 1)
+        (t (+ (tree-count (car tree) (- lvl 1)) 
+              (tree-count (caddr tree) (- lvl 1)))
+        )
+  )
+)
+
+
+;(print (tree-count '(((nil 2 nil) 6 (nil -8 nil)) 7 ((nil 1 nil) 12 nil)) 0))
+;1
+;(print (tree-count '(((nil 2 nil) 6 (nil -8 nil)) 7 ((nil 1 nil) 12 nil)) 1))
+;2
+;(print (tree-count '(((nil 2 nil) 6 (nil -8 nil)) 7 ((nil 1 nil) 12 nil)) 2))
+;3
+;(print (tree-count '(((nil 2 nil) 6 (nil -8 nil)) 7 ((nil 1 nil) 12 nil)) 3))
+;0
+
+
+;      7
+;    /   \
+;  6       12
+; / \     /  
+;2   -8  1    
 
  
-;(print (tree-count '(((nil 2 nil) 6 (nil -8 nil)) 7 (nil 12 nil)) 1))
- 
-;2
-;     7
-;   /  \
-;  6    12
-; / \
-;2   -8
-;(tree-count '(((nil 2 nil) 6 (nil -8 nil)) 7 (nil 12 nil)) 3)
- 
-;0
