502ebbf1f67bcdec1aacc111f4393327b8993e1a
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-04-20 00:21:33 +0300

    Create self-name-return.lisp

---
+++ FUNC/self-name-return.lisp
@@ -0,0 +1,13 @@
+(defmacro defun-this (fname param-list1 &rest body123)
+   `(setf (fdefinition ',fname)
+       (lambda ,param-list1
+          (funcall (lambda (this-f) ,@body123) ',fname))
+    )
+    
+)
+ 
+(defun-this dvprog ()
+         (format t "I am ~a" this-f)
+)
+
+ (dvprog)
