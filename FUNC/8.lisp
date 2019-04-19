bfdc8928335c1b004bf74064e0a4301a3518a25b
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-04-19 14:27:03 +0300

    Update 8.lisp

--- FUNC/8.lisp
+++ FUNC/8.lisp
@@ -1,5 +1,5 @@
 ;Напишите генератор натуральных чисел: 0, 1, 2, 3, 4, 5, ...
-(defun generator (&optional(a 0))
+(defun generator ()
 	(let
 		((numb 0))
 		(lambda () (setq numb (+ numb 1)))
@@ -7,39 +7,31 @@
 )
 
 (defun random-print()
-(if (> (random 10) 5) (print (list(funcall next-number1)))  (print (funcall next-number2))   )
+    (cond 
+        ( (< (print(funcall next-number2)) 10) 
+	  (and(if (> (random 100) 20) (print (list(funcall next-number1)))  (print (funcall next-number2))   )(random-print))
+        )
+        (t  (format t "~%THE END~%")))
 )
 (setq next-number1 (generator)) 
 (setq next-number2 (generator))
 (random-print)
-(random-print)
-(random-print)
-(random-print)
-(random-print)
-(random-print)
-(random-print)
-(random-print)
-(random-print)
-(random-print)
-(random-print)
-(random-print)
-(random-print)
-(random-print)
-(random-print)
 
 ;1 
-;2 
-;3 
 ;(1) 
+;2 
+;(2) 
+;3 
+;(3) 
 ;4 
+;(4) 
 ;5 
 ;6 
 ;7 
-;8 
-;(2) 
-;(3) 
-;(4) 
-;9 
-;10 
 ;(5) 
+;8 
 ;(6) 
+;9 
+;(7) 
+;10 
+;THE END
