69d0b9b3b13e8f463914cbfa9d16b626c6e614e8
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-04-20 18:49:21 +0300

    Update 14.lisp

--- FUNC/14.lisp
+++ FUNC/14.lisp
@@ -1,5 +1,8 @@
+; Определите функцию, которая возвращает в качестве значения
+; форму своего определения (DEFUN).
 (defun thisprog (&rest rest)
           `(defun  thisprog   (&rest rest))
 )
 
 (print(thisprog 'a 'b))
+
