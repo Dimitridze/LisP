9d1460cf14b4630f05641d343df1a0a1bcae17aa
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-05-08 02:11:25 +0300

    Update 2.lisp

--- MACRO/2.lisp
+++ MACRO/2.lisp
@@ -5,14 +5,18 @@
     `(prog1
        (setf first (car ,var))
         (setf var (cdr ,var))
-        (format t "The new value of stack is: ~a" var) 
+        (format t "~%The new value of stack is: ~a" var) 
     )
+    
      
 )
-
-
-
 
 (print(pop1 '(a b c)))
 ;The new value of stack is: (B C)
 ;A 
+(print(pop1 '(b c)))
+;The new value of stack is: (C)
+;B 
+(print(pop1 '(c)))
+;The new value of stack is: NIL
+;C 
