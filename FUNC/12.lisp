25d69565672bb4cf85f7d2f7c3b14a45661b89e9
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-05-09 23:04:27 +0300

    Update 12.lisp

--- FUNC/12.lisp
+++ FUNC/12.lisp
@@ -1,13 +1,12 @@
-;Определите функцию, которая возвращает в качестве значения свой вызов.
+;12.Определите функцию, которая возвращает в качестве значения свой вызов.
 
 (defun self (&rest rest)
 	
 		 
             
-    (append '(self)(mapcan (lambda(x)
-                   (list(list (car(append  ' '(1))) x))) rest))
-	;(append '(self) rest)
+    
+	(append '(self) rest)
 )
 
-(print (self '1 '2)) 
-; (SELF '1 '2) 
+(print (self 1 2)) 
+; (SELF 1 2) 
