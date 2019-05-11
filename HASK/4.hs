2b296e206fc63e9a08e8a69773a732bf176150cc
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-05-11 18:26:32 +0300

    Update 4.hs

--- HASK/4.hs
+++ HASK/4.hs
@@ -2,6 +2,7 @@
 
 separator [] =([],[])
 separator bt = ([x|x<-bt,x<0],[y|y<-bt,y>0])
-main = print $ separator [-1, 2, 0, -7, 4, 5,-6,6,8] 
 
---([-1,-7,-6],[2,4,5,6,8])
+main = print $ separator [-1, 2, 0, -7, 4, 5,-6,6,8]  --([-1,-7,-6],[2,4,5,6,8])
+
+
