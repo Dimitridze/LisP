7245731db406402782cd26ad628bcb2de4de3a61
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-05-11 20:45:55 +0300

    Update 11.hs

--- HASK/11.hs
+++ HASK/11.hs
@@ -1,7 +1,8 @@
 --ghc 7.10
 --11. Чередуя элементы списков [a, b, ...] и [1, 2, ...], образуйте новый список [a, 1, b, 2, ...]
-separator :: [String]->[String]->[String]
-separator [][] = []
-separator (a:at) (b:bt)  = [a] ++ [b] ++  separator at bt
-main = print $ separator  ["a","b"]  ["1","2"]
+alternate :: [String]->[String]->[String]
+alternate [][] = []
+alternate (a:at) (b:bt)  = [a] ++ [b] ++  alternate at bt
+
+main = print $ alternate  ["a","b"]  ["1","2"]
 --["a","1","b","2"]
