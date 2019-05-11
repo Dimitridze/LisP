29ddb2cacb362ca8294ebe986b104a29a782a039
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-05-11 18:23:58 +0300

    Update 6.hs

--- HASK/6.hs
+++ HASK/6.hs
@@ -1,7 +1,8 @@
 --ghc 7.10
-each2Del [] = []
-each2Del (b:btail) =  each2Del btail ++ [b]  
+--7.Записать исходный список в обратном порядке
+backWards [] = []
+backWards (b:btail) = backWards btail ++ [b]  
                     
-main = print $ each2Del [1,2,3,4,5,6,7]
+main = print $ backWards [1,2,3,4,5,6,7]
 
 --[7,6,5,4,3,2,1]
