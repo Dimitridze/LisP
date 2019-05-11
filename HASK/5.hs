adfcc38dcb2d602e88c70ced1afeb4d47fa32ce0
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-05-11 16:25:47 +0300

    Update 5.hs

--- HASK/5.hs
+++ HASK/5.hs
@@ -5,6 +5,10 @@ each2Del (b:[]) = [b]
 each2Del (b:btail) = if (b==head btail)
                      then b : each2Del (drop 1 btail)
                      else b : (each2Del  btail)
-main = print $ each2Del [1,1,2,3,3,4,5,6,7,7,8,9,10]
+main = print $ each2Del [1,2,2,3,4,4,5,6,7,7]
 
---[1,2,3,4,5,6,7,8,9,10]
+--[1,2,3,4,5,6,7]
+
+--main = print $ each2Del [1,1,2,3,3,3,4,5,5,5,5,6]
+
+--[1,2,3,3,4,5,5,6]
