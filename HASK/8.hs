c4c1f222fbaca753937509b1a5a1ff5117131987
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-05-11 18:20:30 +0300

    Update 8.hs

--- HASK/8.hs
+++ HASK/8.hs
@@ -1,9 +1,9 @@
 --ghc 7.10
-8.Увеличть элементы исходного списка на 1
-each2Del :: [Int] -> [Int] 
-each2Del [] = []
-each2Del b = (head b+1):each2Del (drop 1 b)
+--8.Увеличть элементы исходного списка на 1
+plus1 :: [Int] -> [Int] 
+plus1  [] = []
+plus1 b = (head b+1):plus1 (drop 1 b)
                     
-main = print $ each2Del [1,2,3,4,5,4,3,2,1]
+main = print $ plus1 [1,2,3,4,5,4,3,2,1]
 
 --[2,3,4,5,6,5,4,3,2]
