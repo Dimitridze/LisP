486afe9c28e6210420216bf8f58ffa661e2c8a3d
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-05-11 18:25:35 +0300

    Update 5.hs

--- HASK/5.hs
+++ HASK/5.hs
@@ -1,14 +1,14 @@
 --ghc 7.10
-
-each2Del [] = []
-each2Del (b:[]) = [b]
-each2Del (b:btail) = if (b==head btail)
-                     then b : each2Del (drop 1 btail)
-                     else b : (each2Del  btail)
-main = print $ each2Del [1,2,2,3,4,4,5,6,7,7]
+--5.Замена 2 подряд идущих элементов одним
+if2Then1 [] = []
+if2Then1 (b:[]) = [b]
+if2Then1 (b:btail) = if (b==head btail)
+                     then b : if2Then1 (drop 1 btail)
+                     else b : (if2Then1  btail)
+main = print $ if2Then1 [1,2,2,3,4,4,5,6,7,7]
 
 --[1,2,3,4,5,6,7]
 
---main = print $ each2Del [1,1,2,3,3,3,4,5,5,5,5,6]
+--main = print $ if2Then1 [1,1,2,3,3,3,4,5,5,5,5,6]
 
 --[1,2,3,3,4,5,5,6]
