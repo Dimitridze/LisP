435e1ed8510e9459a75640a3081d325a67eb958c
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-05-11 18:21:24 +0300

    Update 7.hs

--- HASK/7.hs
+++ HASK/7.hs
@@ -1,10 +1,10 @@
 --ghc 7.10
-1. Функция замены в списке одного элемента другим.
-each2Del :: (Int) -> (Int) -> [Int] -> [Int] 
-each2Del a1 a2 [] = []
-each2Del a1 a2 b   = if (head b==a1)
-                     then a2 : each2Del a1 a2 (drop 1 b)
-                     else head b : each2Del a1 a2 (drop 1 b)
-main = print $ each2Del 4 9 [4,2,4,3,4,4,5,6,4,4]
+--1. Функция замены в списке одного элемента другим.
+suB :: (Int) -> (Int) -> [Int] -> [Int] 
+suB a1 a2 [] = []
+suB a1 a2 b   = if (head b==a1)
+                     then a2 : suB a1 a2 (drop 1 b)
+                     else head b : suB a1 a2 (drop 1 b)
+main = print $ suB 4 9 [4,2,4,3,4,4,5,6,4,4]
 
 --[9,2,9,3,9,9,5,6,9,9]
