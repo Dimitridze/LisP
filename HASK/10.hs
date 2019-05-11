27c11b2918f22422dd000f246b0961261fac3109
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-05-11 19:50:35 +0300

    Create 10.hs

---
+++ HASK/10.hs
@@ -0,0 +1,15 @@
+--ghc 7.10
+--10. Функция скалярного произведения
+scalar :: [Int] -> [Int] -> Int
+scalar  [][] = 0
+scalar t [] = 0
+scalar [] t = 0
+scalar (a:as) (b:bs) = (a * b) + scalar as bs
+                    
+main = print $ scalar [1,2,3] [10,20,30]
+
+--140
+
+--main = print $ scalar [1,2,3] []
+
+--0
