e7aded6768e4ae3f8a9fc1eaa7bdc1df29447684
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-05-13 15:12:42 +0300

    Update 11.hs

--- HASK/11.hs
+++ HASK/11.hs
@@ -1,8 +1,13 @@
 --ghc 7.10
 --11. Чередуя элементы списков [a, b, ...] и [1, 2, ...], образуйте новый список [a, 1, b, 2, ...]
-alternate :: [String]->[String]->[String]
 alternate [][] = []
 alternate (a:at) (b:bt)  = [a] ++ [b] ++  alternate at bt
 
 main = print $ alternate  ["a","b"]  ["1","2"]
+
 --["a","1","b","2"]
+
+--main = print $ alternate  [1,3] [2,4]  
+
+--[1,2,3,4]
+
