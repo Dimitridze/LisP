8f3a75f93712d1fc120e933ce5070ab41ef06d71
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-05-13 15:15:47 +0300

    Update 12.hs

--- HASK/12.hs
+++ HASK/12.hs
@@ -1,6 +1,6 @@
 --12.Пересечение двух множеств
---inter at bt  = ([x|x<-at,y<-bt,y==x])
+inter1 at bt  = ([x|x<-at,y<-bt,y==x])
 
---main = print $ inter [3,7,6,0,9] [1,3,5,6,9]  
+main = print $ inter [3,7,6,0,9] [1,3,5,6,9]  
 
 --[3,6,9]
