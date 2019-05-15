ff7e5a76d417472ddf889a90e4d5d11e70598285
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-05-15 08:51:34 +0300

    Update 3.lisp

--- MACRO/3.lisp
+++ MACRO/3.lisp
@@ -1,16 +1,13 @@
 ;3. Определите лисповскую форму (IF условие p q) в виде макроса
 
-(defmacro iff (cond1  &rest p)
-    
-     `,(cons cond1 p)
-     
+(defmacro IFF (cond р q)
+    `(if ,cond ,р ,q)
 )
+    
+    
+    
+(setq x '(a b c))
+(print (MYIF (atom x) 'TRUE '(NE TRUE))) ;NE TRUE
+(print (MYIF (listp x) 'Y 'N)) ;YES
 
-(print(iff > 5  4))
-;T
-(print(iff < 5  4))
-;Nil
-(print(iff evenp 6))
-;T
-(print(iff atom '(6 7 8)))
-;Nil
+
