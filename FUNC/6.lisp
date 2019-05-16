f5451cbb726e5d88d73dc648183aabce0185f5dc
Dimitridze <43229773+Dimitridze@users.noreply.github.com>
2019-05-16 08:28:22 +0200

    Update 6.lisp

--- FUNC/6.lisp
+++ FUNC/6.lisp
@@ -9,3 +9,12 @@
 ;(1 3 5)
 (print(del-if 'numberp '( a 1 b 2 c 3)))
 ;(A B C)
+
+
+(defun pl ( lst)
+    (mapcan (lambda(x)
+                   (list(+ x 1)) )lst
+    )
+)
+
+(print(pl  '( 1 2 3 4  )))
